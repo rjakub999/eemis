83846b44cf5ef901db1277e9d2631b9be6f09693
Jakub Rudek <rjakub999@gmail.com>
2020-11-18 00:59:36 +0100

    Uzupelnienie licencji

--- R/emis_calc.R
+++ R/emis_calc.R
@@ -1,3 +1,12 @@
+#' Funkcja testowa na razie tylko mnozy dwa argumenty
+#'
+#' @param x double
+#' @param y double
+#'
+#' @return
+#' @export
+#'
+#' @examples emis_calc(3,4)
 emis_calc <- function(x, y ) {
   return(x * y)
 }
