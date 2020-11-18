a09bd0ae4ee5a62c69833ecaf6368a9b498ab31a
Jakub Rudek <rjakub999@gmail.com>
2020-11-18 01:06:30 +0100

    Poprawka opisu funkcji

--- R/emis_calc.R
+++ R/emis_calc.R
@@ -1,12 +1,13 @@
-#' Funkcja testowa na razie tylko mnozy dwa argumenty
+#' Funkcja testowa mnozy dwa argumenty
 #'
 #' @param x double
 #' @param y double
 #'
-#' @return
+#' @return double
 #' @export
 #'
 #' @examples emis_calc(3,4)
+#'
 emis_calc <- function(x, y ) {
   return(x * y)
 }
